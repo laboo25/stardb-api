2c708aa19a1871c2fba9f3edc3bee1771ef5e447
lab00 <89362256+laboo25@users.noreply.github.com>
2024-05-25 00:12:04 +0600

    Update getAllStar.js

--- controller/getAllStar.js
+++ controller/getAllStar.js
@@ -1,6 +1,6 @@
 // Require the StarBio and StarAlbum models
-const StarBio = require("../models/starBioSchema");
-const StarAlbum = require("../models/albumsSchema");
+// const StarBio = require("../models/starBioSchema");
+// const StarAlbum = require("../models/albumsSchema");
 const newStarSchema = require("../models/newStarSchema");
 
 // Your getAllStar controller function
@@ -8,18 +8,18 @@ async function getAllStar(req, res) {
     try {
         // Retrieve all stars from the database with related models populated as arrays
         const allStars = await newStarSchema.find()
-            .populate({
-                path: 'starImages',
-                model: 'starImage'
-            })
-            .populate({
-                path: 'starbio',
-                model: 'starBio'
-            })
-            .populate({
-                path: 'starAlbums',
-                model: 'starAlbum'
-            });
+            // .populate({
+            //     path: 'starImages',
+            //     model: 'starImage'
+            // })
+            // .populate({
+            //     path: 'starbio',
+            //     model: 'starBio'
+            // })
+            // .populate({
+            //     path: 'starAlbums',
+            //     model: 'starAlbum'
+            // });
 
         res.status(200).json(allStars); // Respond with the array of all stars
     } catch (error) {
