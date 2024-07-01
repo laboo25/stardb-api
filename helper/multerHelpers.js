36c0bcae4f4751056789706ff43bd8ab072d50f8
lab00 <89362256+laboo25@users.noreply.github.com>
2024-07-01 15:49:18 +0600

    Update multerHelpers.js

--- helper/multerHelpers.js
+++ helper/multerHelpers.js
@@ -1,11 +1,13 @@
 // helpers/multerHelpers.js
-
 const multer = require('multer');
 
-// Set up memory storage
 const storage = multer.memoryStorage();
 
-
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: {
+    fileSize: 50 * 1024 * 1024 // Limit file size to 50MB
+  }
+});
 
 module.exports = upload;
