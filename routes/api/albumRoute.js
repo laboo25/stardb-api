6dc46713fa0c18e88e2369f7d226c25ce5391826
lab00 <89362256+laboo25@users.noreply.github.com>
2024-07-02 22:40:52 +0600

    Update albumRoute.js

--- routes/api/albumRoute.js
+++ routes/api/albumRoute.js
@@ -1,15 +1,13 @@
-// routes/api/starBioRoute.js
-
 const express = require('express');
 const router = express.Router();
 const upload = require('../../helper/multerHelpers');
 const createAlbumController = require('../../controller/createAlbumController');
-const { deleteAlbumController, updateAlbumController } = require('../../controller/modifyAlbumController');
+const updateAlbumController = require('../../controller/updateAlbumController');
 const getAllAlbums = require('../../controller/getAllAlbums');
 
 router.post('/create-album', upload.array('albums', 999), createAlbumController);
-router.put('/update', updateAlbumController)
-router.delete('/delete-album', deleteAlbumController)
-router.get('/get-all-albums', getAllAlbums)
+router.put('/update/:albumId', upload.array('albumimages', 999), updateAlbumController);
+// router.delete('/delete-album/:albumId', deleteAlbumController);
+router.get('/get-all-albums', getAllAlbums);
 
-module.exports = router
+module.exports = router;
