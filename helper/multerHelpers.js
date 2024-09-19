c23dd9edbf2a065ee72883655b59e474e1e43e2f
lab00 <89362256+laboo25@users.noreply.github.com>
2024-09-19 20:24:11 +0600

    Update multerHelpers.js

--- helper/multerHelpers.js
+++ helper/multerHelpers.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 
+// Use memory storage for uploading directly to Cloudinary
 const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+
+// Set file size limit to 50MB for each file
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 300 * 1024 * 1024 }, // 50MB per file
+});
 
 module.exports = upload;
